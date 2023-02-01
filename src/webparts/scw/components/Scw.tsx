2b3b51d838c62385f46e2096da05550d7a896f09
Stéphanie Lefebvre <STLEFEBV@tbs-sct.gc.ca>
2023-02-01 13:50:46 -0500

    step form init

--- src/webparts/scw/components/Scw.tsx
+++ src/webparts/scw/components/Scw.tsx
@@ -1,43 +1,67 @@
 import * as React from 'react';
 import styles from './Scw.module.scss';
 import { IScwProps } from './IScwProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import { Steps, Button, message } from 'antd';
+import FistStep from "./FirstStep";
 
-export default class Scw extends React.Component<IScwProps, {}> {
-  public render(): React.ReactElement<IScwProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
-    } = this.props;
+const { Step } = Steps;
 
-    return (
-      <section className={`${styles.scw} ${hasTeamsContext ? styles.teams : ''}`}>
-        <div className={styles.welcome}>
-          <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
-          <h2>Well done, {escape(userDisplayName)}!</h2>
-          <div>{environmentMessage}</div>
-          <div>Web part property value: <strong>{escape(description)}</strong></div>
-        </div>
-        <div>
-          <h3>Welcome to SharePoint Framework!</h3>
-          <p>
-            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-          </p>
-          <h4>Learn more about SPFx development:</h4>
-          <ul className={styles.links}>
-            <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">Microsoft 365 Developer Community</a></li>
-          </ul>
-        </div>
-      </section>
-    );
-  }
+const steps = [
+    {
+        title: 'First',
+        content: <FistStep></FistStep>,
+    },
+    {
+        title: 'Second',
+        content: 'Second-content',
+    },
+    {
+        title: 'Last',
+        content: 'Last-content',
+    },
+];
+
+export interface IScwState {
+    current: number;
+}
+
+export default class AntDesignStep extends React.Component<IScwProps, IScwState> {
+
+    public constructor(props: IScwProps, state: IScwState) {
+        super(props);
+        this.state = {
+            current: 0
+        };
+    }
+
+    private next = () => {
+        const current = this.state.current + 1;
+        this.setState({ current });
+    }
+
+    private prev = () => {
+        const current = this.state.current - 1;
+        this.setState({ current });
+    }
+
+    public render(): React.ReactElement<IScwProps> {
+        return (
+            <div className={styles.scw}>
+                <div className={styles.container}>
+                    <div className={styles.row}>
+                        <Steps current={this.state.current}>
+                            {steps.map(item => (<Step key={item.title} title={item.title} />))}
+                        </Steps>
+                        <div className="steps-content">{steps[this.state.current].content}</div>
+                        <div className="steps-action">
+                            {this.state.current < steps.length - 1 && (<Button type="primary" onClick={() => this.next()}>Next</Button>)}
+                            {this.state.current === steps.length - 1 && (<Button type="primary" onClick={() => message.success('Processing complete!')}>Done</Button>)}
+                            {this.state.current > 0 && (<Button style={{ margin: '0 8px' }} onClick={() => this.prev()}>Previous</Button>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
