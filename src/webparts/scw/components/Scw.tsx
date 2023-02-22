9ccd03655b5ed560b16d6b53000237282509593f
gabmor38 <gabriela.morenor@gmail.com>
2023-02-22 12:13:23 -0500

    removed steps at the beginning

--- src/webparts/scw/components/Scw.tsx
+++ src/webparts/scw/components/Scw.tsx
@@ -8,61 +8,12 @@ import { Initial } from './InitialPage/Initial';
 import { IButtonStyles, PrimaryButton } from 'office-ui-fabric-react';
 import LastStep from './LastStep';
 
-// const { Step } = Steps;
-
-
-
-// const steps = [
-//     {
-//         step: '1',
-//         title: '1',
-//         content: <FirstStep name={this.handleCallback}/>,
-//     },
-//     {
-//         title: '2',
-//         content: 'Second-content',
-//     },
-//     {
-//         title: '3',
-//         content: 'Third-content',
-//     },
-//     {
-//         title: '4',
-//         content: 'Fourth-content',
-//     },
-//     {
-//         title: '5',
-//         content: 'Fifth-content',
-//     },
-//     {
-//         title: '6',
-//         content: 'Sixth-content',
-//     },
-//     {
-//         title: '7',
-//         content: 'Seventh-content',
-//     },
-//     {
-//         title: '8',
-//         content: 'Last-content',
-//         // status: 'finished'
-//     },
-//     {
-//         title: '9',
-//         content: 'Last-content',
-//         status: 'finished'
-//     }
-// ];
-
 
 export interface IScwState {
     current: number;
     step: number;
     name: string;
-   
-    
 }
-
 
 
 export default class AntDesignStep extends React.Component<IScwProps, IScwState> {
@@ -71,40 +22,26 @@ export default class AntDesignStep extends React.Component<IScwProps, IScwState>
         this.state = {
             current: 0,
             step: 0,
-            name:'',
-            
+            name:'',   
         };
-
         this.handleCallback = this.handleCallback.bind(this);
     }
-
-    // componentDidMount(): void {
-    //     const getName = localStorage.getItem('name');
-    //     console.log("getN",getName)
-    //     const parseData = JSON.parse(getName);
-    //     if(getName) {
-    //         this.setState({name: parseData});
-    //     }
-    // }
- 
 
     private next = (): void => {
         const current = this.state.current + 1;
         const saveName = this.state.name;
-        const getName = localStorage.getItem(JSON.stringify('name'));
+        // const getName = localStorage.getItem(JSON.stringify('name'));
         this.setState({ current: current, name: saveName });
-
-        console.log("name", getName)
     }
 
     private prev = (): void => {
         
         const current = this.state.current - 1;
-        const prevValue = localStorage.getItem("name")
+        // const prevValue = localStorage.getItem("name")
         
-        if(prevValue) {
-            this.setState({name: prevValue});
-        }
+        // if(prevValue) {
+        //     this.setState({name: prevValue});
+        // }
         this.setState({ current });   
     }
 
@@ -130,7 +67,6 @@ export default class AntDesignStep extends React.Component<IScwProps, IScwState>
             })
         ) 
     }
-
 
     public handleCallback = (name: string): void => {
         // localStorage.setItem('name', name); 
