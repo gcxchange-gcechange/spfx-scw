e674bdca226ddde70a865987eef85eaf23cb398e
gabmor38 <gabriela.morenor@gmail.com>
2023-03-03 23:07:24 -0500

    refactored functions into one

--- src/webparts/scw/components/FirstStep.tsx
+++ src/webparts/scw/components/FirstStep.tsx
@@ -16,12 +16,7 @@ export interface IFirstStepProps {
     shFrDesc: string;
     errorMessage: string;
 
- 
-    handleEngNameCallback?:(engNameValue: string) => void;
-    frNameCallback?:(FrNameValue: string) => void;
-    engDescCallback?:(shEngDescValue: string) => void;
-    frDescCallback?: (shFrDescValue: string) => void;
-    commPurposeCallback?: (commPurpose: string) => void;
+    handleOnChange?:(event:any, value: string)=> void;
     handleErrorMessage?: (errorMessage: string) => void;
 
 
@@ -52,73 +47,46 @@ export default class FirstStep extends React.Component<IFirstStepProps> {
             <p>The GCX Support Team needs to know the purpose of the new community to determine whether it can be <strong>approved.</strong></p>
             <Label htmlFor='Community purpose' required>Community purpose</Label>
             <p className={ styles.instruction }>This will not show up on your site. Write in the official language of your choice. Max. 500 characters</p>
-            <TextField type='text' name='commPurpose' id='Community purpose'  onChange={ this.commPurposeOnChange }defaultValue={ commPurpose } />
+            <TextField type='text' name='commPurpose' id='Community purpose'  onChange={ this.onhandleChangeEvent }defaultValue={ commPurpose } />
 
             
             <h2>Community name</h2>
             <p>The communitys name is the title of your community. Create a short descriptive name. A bilingual name complies with the Official Langauges Act and makes it easier for others to find your community in the GCXchange Catalogue.</p>
             <Label htmlFor={labels[0]} required>English community name</Label>
             <p className={ styles.instruction }>Use keywords, not abbreviations for better discoverability. Must be between 5 and 126 characters in length and special characters are not permitted.</p>
-            <TextField  id={labels[0]} name='EngName' onChange={this.handleOnChangeEngNameEvent} defaultValue={engName} onGetErrorMessage={this.onGetErrorMessage} validateOnLoad={false}/>
+            <TextField  id={labels[0]} name='engName' onChange={this.onhandleChangeEvent} defaultValue={engName} onGetErrorMessage={this.onGetErrorMessage} validateOnLoad={false}/>
             <Label htmlFor='frCommName' required>French community name</Label>
             <p className={ styles.instruction }>Use keywords, not abbreviations for better discoverability. Must be between 5 and 126 characters in length and special characters are not permitted.</p>
             <p className={ styles.instruction }>Max. 33 characters.</p>
-            <TextField  id='frCommName' name='frCommName' onChange={this.OnChangeFrNameEvent } defaultValue={frCommName}/>
+            <TextField  id='frCommName' name='frCommName' onChange={this.onhandleChangeEvent } defaultValue={frCommName}/>
 
 
             <h2>Community description</h2>
             <p>The community descriptions will be visible to users when they use the {`"All communities"`} page and when they search for it.</p>
             <Label htmlFor='shEngDesc' required>English description name</Label>
             <p className={ styles.instruction }>Max. 33 characters.</p>
-            <TextField id='shEngDesc' name='shEngDesc'onChange={this.handleOnChangeEngDescEvent} defaultValue={shEngDesc}/>
+            <TextField id='shEngDesc' name='shEngDesc'onChange={this.onhandleChangeEvent} defaultValue={shEngDesc}/>
 
             <Label htmlFor='shFrDesc' required>French description name</Label>
             <p className={ styles.instruction }>Max. 33 characters.</p>
-            <TextField id='shFrDesc' name='shFrDesc' onChange={this.handleOnChangeFrDescEvent} defaultValue={shFrDesc}/> 
+            <TextField id='shFrDesc' name='shFrDesc' onChange={this.onhandleChangeEvent} defaultValue={shFrDesc}/> 
             
             </>
         );
     }
 
-
-    private  commPurposeOnChange = (event:React.ChangeEvent<HTMLInputElement>) :void => { 
-        const value = event.target.value;
-        this.setState({commPurpose: value});
-        //send it to the parent
-        this.props.commPurposeCallback(value)
-    }
-
-    private  handleOnChangeEngNameEvent = (event:React.ChangeEvent<HTMLInputElement>) :void => { 
-        const nameValue = event.target.value;
-        this.setState({engName: nameValue});
-        //send it to the parent
-        this.props.handleEngNameCallback(nameValue)
+    private onhandleChangeEvent = (event: React.ChangeEvent<HTMLInputElement>) :void => {
+        const eventValue = event.target.value;
+        console.log(event.target.name)
+        this.setState({
+            ...this.state,
+            [event.target.name]: eventValue
+        })
+        this.props.handleOnChange(event.target.name, eventValue)
     }
 
 
-    private  OnChangeFrNameEvent = (event: React.ChangeEvent<HTMLInputElement>) :void => { 
-        const frNameValue = event.target.value;
-        this.setState({frCommName: frNameValue});
-        //send it to the parent
-        this.props.frNameCallback(frNameValue)
-       
-    }
 
-
-    private  handleOnChangeEngDescEvent = (event: React.ChangeEvent<HTMLInputElement>) :void => { 
-        const engDescValue = event.target.value;
-        this.setState({shEngDesc: engDescValue});
-        //send it to the parent
-        this.props.engDescCallback(engDescValue)
-       
-    }
-    private  handleOnChangeFrDescEvent = (event: React.ChangeEvent<HTMLInputElement>) :void => { 
-        const frDescValue = event.target.value;
-        this.setState({shFrDesc: frDescValue});
-        //send it to the parent
-        this.props.frDescCallback(frDescValue)
-       
-    }
 
     public onGetErrorMessage = ():JSX.Element => {
         
