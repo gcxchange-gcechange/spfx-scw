c8712f5d3efe25e035ed100c0cde98546aea753d
gabmor38 <gabriela.morenor@gmail.com>
2023-07-19 15:20:21 -0400

    moved asteriks to beginging of lable and added an aria-Label =required to them

--- src/webparts/scw/components/FirstStep.tsx
+++ src/webparts/scw/components/FirstStep.tsx
@@ -46,6 +46,7 @@ export default class FirstStep extends React.Component<IFirstStepProps> {
             root: {
                 paddingBottom: '5px',
             },
+            
 
            
         }
@@ -57,7 +58,10 @@ export default class FirstStep extends React.Component<IFirstStepProps> {
            
             <h3>{ parse( this.strings.commPurpose_title ) }</h3>
             <p>{ parse( this.strings.commPurpose_desc) }</p>
-            <Label htmlFor='Community purpose' required styles={ labelStyle }>{ this.strings.commPurpose_title }</Label>
+            <Label htmlFor='Community purpose'styles={ labelStyle } >
+                <span className={styles.asterik}  aria-label='required'>*</span>
+                { this.strings.commPurpose_title }
+            </Label>
             <p className={ styles.instruction }>{ this.strings.commPurpose_Instruction}</p>
             
             <TextField type='text' name='commPurpose' id='Community purpose' multiline rows={3} onChange={ this.onhandleChangeEvent } 
@@ -68,13 +72,19 @@ export default class FirstStep extends React.Component<IFirstStepProps> {
             <p className={ styles.topMgn0 }>{ this.strings.engName_desc}</p>
             <Stack tokens={ stackTokens }>
                 <StackItem>
-                    <Label htmlFor='engName' required styles={ labelStyle } >{ this.strings.engName_title }</Label>
+                    <Label htmlFor='engName' styles={ labelStyle } >
+                        <span className={styles.asterik}  aria-label='required'>*</span>
+                        { this.strings.engName_title }
+                    </Label>
                     <p className={ styles.instruction }>{ this.strings.engName_Instruction}</p>
                     <TextField  id='engName' name='engName' onChange={ this.onhandleChangeEvent } defaultValue={ engName }  validateOnLoad= { false }  
-                    onGetErrorMessage={ this.validateInput }/>
+                    onGetErrorMessage={ this.validateInput } />
                 </StackItem>
                 <StackItem>
-                    <Label htmlFor='frCommName' required>{ this.strings.frCommName_title }</Label>
+                    <Label htmlFor='frCommName'>
+                        <span className={styles.asterik}  aria-label='required'>*</span>
+                        {this.strings.frCommName_title }
+                    </Label>
                     <p className={ styles.instruction }>{ this.strings.frCommName_Instruction}</p>
                     <TextField id='frCommName' name='frCommName' onChange={ this.onhandleChangeEvent } defaultValue={ frCommName } validateOnLoad= { false } onGetErrorMessage={ this.validateInput } />
                 </StackItem>
@@ -84,12 +94,18 @@ export default class FirstStep extends React.Component<IFirstStepProps> {
             <p className={ styles.topMgn0 }> { this.strings.shEngDesc_desc }</p>
             <Stack tokens={ stackTokens }>
                 <StackItem>
-                    <Label htmlFor='shEngDesc' required  styles={ labelStyle } >{ this.strings.shEngDesc_title }</Label>
+                    <Label htmlFor='shEngDesc' styles={ labelStyle } >
+                        <span className={styles.asterik}  aria-label='required'>*</span>
+                        { this.strings.shEngDesc_title }
+                    </Label>
                     <p className={ styles.instruction }>{ this.strings.shEngDesc_Instruction }</p>
                     <TextField id='shEngDesc' name='shEngDesc'onChange={ this.onhandleChangeEvent} defaultValue={ shEngDesc } validateOnLoad= { false }  onGetErrorMessage={ value => { if (value.length > 33 || value === '') return `${this.strings.max33_validation}` }} />
                 </StackItem>
                 <StackItem>
-                    <Label htmlFor='shFrDesc' required  styles={ labelStyle } >{ this.strings.shFrDesc_title }</Label>
+                    <Label htmlFor='shFrDesc' styles={ labelStyle } >
+                        <span className={styles.asterik}  aria-label='required'>*</span>
+                        { this.strings.shFrDesc_title }
+                    </Label>
                     <p className={ styles.instruction }>{ this.strings.shFrDesc_Instruction }</p>
                     <TextField id='shFrDesc' name='shFrDesc' onChange={ this.onhandleChangeEvent} defaultValue={ shFrDesc } validateOnLoad= { false }  onGetErrorMessage={ value => { if (value.length > 33 || value === '') return `${this.strings.max33_validation}` }}/> 
                 </StackItem>
--- src/webparts/scw/components/FourthStep.tsx
+++ src/webparts/scw/components/FourthStep.tsx
@@ -89,7 +89,10 @@ export default class FourthStep extends React.Component<IFourthStepProps> {
                 <p>{ parse( this.strings.invite_owners_para2 ) }</p>
                 {/* <p>{ parse( this.strings.invite_owners_para3 ) }</p> */}
 
-                <Label>{this.strings.invite_owners_label}</Label>
+                <Label>
+                    <span className={styles.asterik}  aria-label='required'>*</span>
+                    {this.strings.invite_owners_label}
+                </Label>
                 <p className={ styles.instruction }>{ this.strings.owners_Instruction}</p>
                 <AddUsers 
                 prefLang={this.props.prefLang}
