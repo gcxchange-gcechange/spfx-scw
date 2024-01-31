eecb90d3d05426ef15cc9deca5145260071d0123
gabmor38 <gabriela.morenor@gmail.com>
2024-01-31 14:22:01 -0500

    removed unused code

--- src/webparts/scw/components/Scw.tsx
+++ src/webparts/scw/components/Scw.tsx
@@ -225,7 +225,7 @@ export default class AntDesignStep extends React.Component<IScwProps, IScwState>
         }
 
         else {
-            const functionUrl = "https://appsvc-fnc-dev-scw-list-dotnet001.azurewebsites.net/api/CreateItem?";
+            const functionUrl = "CreateItem?";
             const requestHeaders: Headers = new Headers();
             requestHeaders.append("Content-type", "application/json");
             requestHeaders.append("Cache-Control", "no-cache");
@@ -278,7 +278,7 @@ export default class AntDesignStep extends React.Component<IScwProps, IScwState>
             document.getElementById("submit").style.display = 'none';
                 
               this.props.context.aadHttpClientFactory
-              .getClient("3385e8cd-40a4-41f5-bd2f-68690654a54b")
+              .getClient("")
               .then((client: AadHttpClient) => {
                
                 client.post(functionUrl, AadHttpClient.configurations.v1, postOptions)
