532e250f59b3ccc421d61a6281fc230302e9ef2c
gabmor38 <gabriela.morenor@gmail.com>
2024-05-15 16:15:00 -0400

    changed auth

--- src/webparts/scw/components/Scw.tsx
+++ src/webparts/scw/components/Scw.tsx
@@ -275,7 +275,7 @@ export default class AntDesignStep extends React.Component<
     if (showModal) {
       this.setState({ showModal: true });
     } else {
-      const functionUrl = "https://appsvc-fnc-dev-scw-list-dotnet001.azurewebsites.net/api/CreateItem?";
+      const functionUrl = "CreateItem?";
       const requestHeaders: Headers = new Headers();
       requestHeaders.append("Content-type", "application/json");
       requestHeaders.append("Cache-Control", "no-cache");
@@ -311,12 +311,12 @@ export default class AntDesignStep extends React.Component<
         document.getElementById("submit").style.display = "none";
 
         this.props.context.aadHttpClientFactory
-          .getClient("3385e8cd-40a4-41f5-bd2f-68690654a54b")
+          .getClient(" ")
           .then((client: AadHttpClient) => {
             client
               .post(functionUrl, AadHttpClient.configurations.v1, postOptions)
               .then((response: HttpClientResponse) => {
-                console.log(`Status code: ${response.status}`);
+                console.log(`Status code: ${response}`);
 
                 if (response.status) {
                   this.setState({
